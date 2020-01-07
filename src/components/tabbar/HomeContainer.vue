82775b48ff9b12b4b244c0c50564b0d85204dd13
cenmom <1370375698@qq.com>
2020-01-07 17:52:48 +0800

    新增路由

---
+++ src/components/tabbar/HomeContainer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="zz">
+    <h3>HomeContainer</h3>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang='scss' scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -2,14 +2,21 @@ import MintUI from 'mint-ui'
 
 import './lib/mui/css/mui.min.css'
 
+
 import Vue from 'vue'
 import 'mint-ui/lib/style.css'
 import app from './App.vue'
+import VueRouter from 'vue-router'
+import router from './router.js'
+
+Vue.use(VueRouter)
+
 
 Vue.use(MintUI)
 
 
 var vm = new Vue({
     el: '#app',
-    render: c => c(app)
+    render: c => c(app),
+    router
 })
